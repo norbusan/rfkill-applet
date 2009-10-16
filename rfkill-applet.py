cc3d37978e1b555eca1674e8b9bef3a4188a0340
Norbert Preining <preining@logic.at>
2009-10-17 00:17:56 +0900

    importing version 0.6

--- rfkill-applet.py
+++ rfkill-applet.py
@@ -9,18 +9,192 @@ import sys
 import os
 import pygtk
 pygtk.require('2.0')
-import threading
 import gtk
 import gnomeapplet
+import gobject
+import struct
+import dbus
 
-import rfkillclient
+version = '0.6'
 
-version = '0.5'
+
+event_format = "IBBBB"
+
+RFKILL_OP_ADD = 0
+RFKILL_OP_DEL = 1
+RFKILL_OP_CHANGE = 2
+RFKILL_OP_CHANGE_ALL = 3
+
+class RfkillAccess():
+  AccessO = None
+
+  def __init__ (self, applet, ignore):
+    self.applet = applet
+    self.rfkill_names = dict()
+    self.rfkill_hardstates = dict()
+    self.rfkill_softstates = dict()
+    try:
+      f = os.open("/dev/rfkill", os.O_RDWR)
+      os.close(f)
+      self.AccessO = RfkillAccessDevRfkill(applet, ignore)
+      print "Using /dev/rfkill access mode"
+
+    except:
+      self.AccessO = RfkillAccessDbus(applet, ignore)
+      print "Using Hal/DBUS access mode"
+
+  def get_state (self, idx):
+    return self.AccessO.get_state(idx)
+
+  def toggle_softstate (self, idx):
+    return self.AccessO.toggle_softstate (idx)
+
+  def get_rfkillall (self):
+    return self.AccessO.get_rfkillall()
+
+
+class RfkillAccessDbus():
+  def __init__ (self, applet, ignore):
+    self.applet = applet
+    self.ignored = dict()
+    for k,v in ignore.iteritems():
+      if v:
+        self.ignored[k] = 1
+    self.bus = dbus.SystemBus()
+    self.hal_obj = self.bus.get_object("org.freedesktop.Hal", "/org/freedesktop/Hal/Manager")
+    self.hal = dbus.Interface(self.hal_obj, "org.freedesktop.Hal.Manager")
+    self.rfkill_names = dict()
+    self.rfkill_hardstate = dict()
+    self.rfkill_softstate = dict()
+    # set up timeout for restarting
+    gobject.timeout_add(3000, self.periodic_check)
+
+  def periodic_check (self):
+    self.parent_set_hard_switch()
+    return True
+
+  def parent_set_hard_switch (self):
+    # will only be called in HAL/DBUS mode
+    # we have to check for the actual hard switches
+    saved_hard = self.rfkill_hardstate
+    allrf = self.get_rfkillall()
+    is_hard_off = False
+    for idx, name in self.get_rfkillall().iteritems():
+      if not(name in self.ignored):
+        newhard, newsoft = self.get_state(idx)
+        if newhard:
+          is_hard_off = True
+    self.applet.set_hard_switch(is_hard_off)
+      
+  def get_rfkillall (self):
+    self.rfkill_names = dict()
+    self.rfkill_hardstate = dict()
+    self.rfkill_softstate = dict()
+    self.rfkill_devs = dict()
+    for udi in self.hal.FindDeviceByCapability ("killswitch"):
+      dev_obj = self.bus.get_object('org.freedesktop.Hal', udi)
+      dev = dbus.Interface(dev_obj, 'org.freedesktop.Hal.Device')
+      if (dev.GetProperty('killswitch.type') != "unknown"):
+        name = str(dev.GetProperty ('killswitch.name'))
+        if (name in self.ignored):
+          continue
+        self.rfkill_names[udi] = name
+        val = int(dev.GetProperty ('killswitch.state'))
+        self.rfkill_devs[udi] = dev_obj
+        if (val == 1):
+          self.rfkill_hardstate[udi] = 0
+          self.rfkill_softstate[udi] = 0
+        elif (val == 2):
+          self.rfkill_softstate[udi] = 0
+          self.rfkill_hardstate[udi] = 1
+        else:
+          self.rfkill_softstate[udi] = 1
+          self.rfkill_hardstate[udi] = 0
+    return (self.rfkill_names)
+ 
+  def get_state (self, idx):
+    if self.rfkill_names[idx]:
+      return(self.rfkill_hardstate[idx], self.rfkill_softstate[idx])
+    else:
+      return None
+
+  def toggle_softstate (self, idx):
+    dev = dbus.Interface(self.rfkill_devs[idx], 'org.freedesktop.Hal.Device.KillSwitch')
+    # the value is already inverted, so no need for not
+    dev.SetPower(self.rfkill_softstate[idx])
+
+
+
+
+class RfkillAccessDevRfkill():
+  def __init__(self, applet, ignore):
+    self.ignored = dict()
+    self.applet = applet
+    for k,v in ignore.iteritems():
+      if v:
+        self.ignored[k] = 1
+    self.rfkillfd = os.open("/dev/rfkill", os.O_RDONLY)
+    gobject.io_add_watch(self.rfkillfd, gobject.IO_IN, self.callback_event)
+    self.rfkill_names = dict()
+    self.rfkill_hardstate = dict()
+    self.rfkill_softstate = dict()
+
+  def callback_event (self, fd, condition):
+    buf = os.read(self.rfkillfd, 8)
+    if (len(buf) != 8):
+      print "cannot read full event from fd"
+    else:
+      (idx, type, op, soft, hard) = struct.unpack(event_format, buf)
+      if op == RFKILL_OP_DEL:
+        if (idx in self.rfkill_names):
+          del self.rfkill_names[idx]
+        return True
+      if op == RFKILL_OP_ADD:
+        # name should be set from /sys/class/rfkill/rfkill%N/name
+        f = open("/sys/class/rfkill/rfkill" + str(idx) + "/name")
+        name = f.readline().rstrip()
+        f.close()
+        if not(name in self.ignored):
+          self.rfkill_names[idx] = name
+
+      if (idx in self.rfkill_names):
+        # otherwise the rfkill switch is ignored
+
+        # we want to check whether a hard switch was toggled
+        # this is done by checking whether the property hard has
+        # changed
+        if (idx in self.rfkill_hardstate):
+          if (hard != self.rfkill_hardstate[idx]):
+            # inform parent that we have a hard switch toggle
+            self.applet.set_hard_switch(hard)
+        self.rfkill_hardstate[idx] = hard
+        self.rfkill_softstate[idx] = soft
+
+    return True
+
+  def parent_set_hard_switch (self):
+    return None
+      
+  def get_rfkillall (self):
+    return (self.rfkill_names)
+ 
+  def get_state (self, idx):
+    if self.rfkill_names[idx]:
+      return(self.rfkill_hardstate[idx], self.rfkill_softstate[idx])
+    else:
+      return None
+
+  def toggle_softstate (self, idx):
+    buf = struct.pack(event_format, idx, 0, RFKILL_OP_CHANGE, 
+                      not(self.rfkill_softstate[idx]), 0)
+    writefd = os.open("/dev/rfkill", os.O_RDWR)
+    if (os.write(writefd, buf) < 8):
+      print "Cannot write to rfkill the full event type"
+    os.close(writefd)
+ 
+
 
 class Rfkill:
-
-  thread = None
-
   def __init__(self, applet, iid):
 
     self.configfile = os.environ.get('HOME') + '/.rfkill-applet.config'
@@ -67,18 +241,11 @@ class Rfkill:
     self.applet.setup_menu(self.menuxml, [ ("About",self.about_box), ("Preferences",self.prefs), ("Quit", self.cleanup) ], None)
 
 
-    # start the thread that reads/writes to /dev/rfkill
-    if self.thread != None:
-      self.thread.kill()
-      self.thread = None
-    self.thread = rfkillclient.RfkillClient(self, self.config_ignore)
-    self.thread.start()
+    self.AccessO = RfkillAccess(self, self.config_ignore)
 
-    # read all data from the rfkill thread and initialize the lists
     self.update_all()
 
     applet.show_all()
-    gtk.gdk.threads_init()
     # self.load_prefs()
   
   def update_all(self):
@@ -88,19 +255,18 @@ class Rfkill:
     self.rfkills_idx = []
     self.rfkills_showname = []
     self.hardswitchedoff = False
-    if self.thread != None:
-      for idx, name in self.thread.get_rfkillall().iteritems():
-        hard, soft = self.thread.get_state(idx)
-        if hard:
-          self.hardswitchedoff = True
-        self.rfkills_hard.append(hard)
-        self.rfkills_soft.append(soft)
-        self.rfkills_name.append(name)
-        self.rfkills_idx.append(idx)
-        if (name in self.config_names):
-          self.rfkills_showname.append(self.config_names[name])
-        else:
-          self.rfkills_showname.append(name)
+    for idx, name in self.AccessO.get_rfkillall().iteritems():
+      hard, soft = self.AccessO.get_state(idx)
+      if hard:
+        self.hardswitchedoff = True
+      self.rfkills_hard.append(hard)
+      self.rfkills_soft.append(soft)
+      self.rfkills_name.append(name)
+      self.rfkills_idx.append(idx)
+      if (name in self.config_names):
+        self.rfkills_showname.append(self.config_names[name])
+      else:
+        self.rfkills_showname.append(name)
     self.set_main_icon()
     self.update_tooltip()
     
@@ -187,10 +353,9 @@ class Rfkill:
     self.update_tooltip()
 
   def toggle_rfkill (self, widget, idx):
-    self.thread.toggle_softstate(self.rfkills_idx[idx])
+    self.AccessO.toggle_softstate(self.rfkills_idx[idx])
   
   def cleanup(self, a, b):
-    self.thread.kill()
     gtk.main_quit()
     sys.exit()
 
@@ -216,5 +381,6 @@ if __name__ == '__main__':
                              gnomeapplet.Applet.__gtype__, 
                              "RFKill Switch Applet", "0.1", 
                              rfkill_factory)
+
 
 # vim:set tabstop=2 expandtab: #
